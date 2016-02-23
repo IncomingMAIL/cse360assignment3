14515f63dab3fbc4ce9fe3facabe8635aa0e46e9
IncomingMAIL <quinnsmith4@gmail.com>
2016-02-22 22:54:39 -0700

    initial commit
    N/a

---
+++ src/cse360assign3/Calculator.java
@@ -0,0 +1,75 @@
+package cse360assign3;
+
+/**
+  * Calculator object for arithmetic operations
+  * @author Benjamin Smith (PIN 810) CSE 360 Spring 2016
+  * @version February 22, 2016
+  */
+public class Calculator {
+
+	/**int for use in calculations. */
+	private int total;
+	
+	/**
+      * Constructor for initializing the calculator object.
+	  * @author Benjamin Smith (PIN 810) CSE 360 Spring 2016
+      * @version February 22, 2016
+      */
+	public Calculator () {
+		total = 0;  // not needed - included for clarity
+	}
+	
+	/**
+	 * Gets the calculator's total.
+	 * @return Calculator total result
+	 * @author Benjamin Smith (PIN 810) CSE 360 Spring 2016
+	 */
+	public int getTotal () {
+		return 0;
+	}
+	
+	/**
+	 * Adds the value to total.
+	 * @param value Value to add to total
+	 * @author Benjamin Smith (PIN 810) CSE 360 Spring 2016
+	 */
+	public void add (int value) {
+		
+	}
+	
+	/**
+	 * Subtracts the value from total
+	 * @param value Value to subtract from total
+	 * @author Benjamin Smith (PIN 810) CSE 360 Spring 2016
+	 */
+	public void subtract (int value) {
+		
+	}
+	
+	/**
+	 * Multiply total by value
+	 * @param value Value to multiply by
+	 * @author Benjamin Smith (PIN 810) CSE 360 Spring 2016
+	 */
+	public void multiply (int value) {
+		
+	}
+	
+	/**
+	 * Divides total by value
+	 * @param value Value to divide by
+	 * @author Benjamin Smith (PIN 810) CSE 360 Spring 2016
+	 */
+	public void divide (int value) {
+		
+	}
+	
+	/**
+	 * Returns string containing the calculator's history
+	 * @return String containing the calculator's history
+	 * @author Benjamin Smith (PIN 810) CSE 360 Spring 2016
+	 */
+	public String getHistory () {
+		return "";
+	}
+}
